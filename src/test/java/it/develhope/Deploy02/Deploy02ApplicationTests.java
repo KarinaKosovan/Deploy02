46144c89260f363428dc5376c4f39ebbcbf8547e
Karina Kosovan <karikosovan@gmail.com>
2022-10-24 15:23:54 +0200

    Initial commit

---
+++ src/test/java/it/develhope/Deploy02/Deploy02ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.Deploy02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
