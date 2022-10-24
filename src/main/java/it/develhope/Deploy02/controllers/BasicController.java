a10bb9815d14946d253839076d8b38b7cc085b24
Karina Kosovan <karikosovan@gmail.com>
2022-10-24 17:54:52 +0200

    Fixing yml

--- src/main/java/it/develhope/Deploy02/controllers/BasicController.java
+++ src/main/java/it/develhope/Deploy02/controllers/BasicController.java
@@ -1,5 +1,7 @@
 package it.develhope.Deploy02.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
